ce1679a1226f3456afec7713104e47715c63d1e3
Mohamed Arafa <mohaalp_salah@yahoo.com>
2022-11-05 15:34:04 +0300

    merge

---
+++ Portal_and_Admin_UI/src/environments/environment.staging.ts
@@ -0,0 +1,27 @@
+export const environment = {
+  production: false,
+
+  ApiUrl: "http://localhost:4201",
+  lastSieUpdate: "2022/10/30",
+
+  allowedExtensions: 'image/*',
+  fileSize: 4000000,
+
+  auctionsallowedExtensions: 'image/*',
+  auctionsfileSize: 2000000,
+  governoratesallowedExtensions: 'image/*',
+  governoratesfileSize: 2000000,
+  newsallowedExtensions: 'image/*',
+  newsfileSize: 1500000,
+  postersallowedExtensions: 'image/*',
+  postersfileSize: 3000000,
+  princesallowedExtensions: 'image/*',
+  princesfileSize: 2000000,
+  servicesallowedExtensions: 'image/*',
+  servicesfileSize: 2000000,
+  serviceExplainAllowedExtensions: 'image/*',
+  serviceExplainFileSize: 6000000,
+  openDataAllowedExtensions: '.xlsx,.xlsm,.xlsb,.xls,.xlt',
+  openDataFileSize: 6000000,
+
+};
